31bc8e5337dee6bb8e78cdef6420d4f079b6b58d
Scarlett <scarlett@dhcp-128-32-21-195.law.berkeley.edu>
2017-01-20 11:41:47 -0800

    home page shows info but no command works outside cmd

--- awesome/home.php
+++ awesome/home.php
@@ -2,19 +2,46 @@
 <html>
 <head>
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!--   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ -->  <!-- Optional theme -->
+<!--   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"> -->  
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/bootstrap-theme.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="/js/bootstrap.min.js"></script>
+  <script src="/js/npm.js"></script>
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
 </head>
 <body>
   <div class="row">
     <div class="col-lg-3 col-lg-offset-4">
-      <iframe height="250px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRnDf68Fo9XxPsdJNlT29ycal1tHWcZ2oxae89HGy9ol0_t3xjFNXcJM_4hFPr0FMUBq6X7BffkfEaS/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      <iframe height="300px" width="500px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRnDf68Fo9XxPsdJNlT29ycal1tHWcZ2oxae89HGy9ol0_t3xjFNXcJM_4hFPr0FMUBq6X7BffkfEaS/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-3 col-lg-offset-4">
+      <div>
+        <?php
+          //checking if the four inputs are set
+          if (isset($_POST['enrollment_id'])   &&
+              isset($_POST['student_id']))   
+          {
+            $enrollment_id   = get_post($conn, 'enrollment_id');
+            $student_id    = get_post($conn, 'student_id');
+          }
+        ?> 
+        <form action="index.php" method="post">
+          <ul style="list-style: none;">
+            <li>First Name <input type="text" name="enrollment_id"></li>
+            <li>Last Name <input type="text" name="student_id"></li>
+            <li>Delete <input type="text" name="course_id"></li>
+            <input type="hidden" name="update">
+            <li><input onclick="index.php" type="submit"></li>
+          </ul>
+        </form>
+      </div>
     </div>
   </div>
 </body>
 </html>
-
--- awesome/oauth2callback.php
+++ awesome/oauth2callback.php
@@ -16,53 +16,7 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   // $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
   // header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 	echo "This is where I can put info i guess";
-
-	// Get the API client and construct the service object.
-	$client = getClient();
-	$service = new Google_Service_Sheets($client);
-
-	$spreadsheetId = '1glCC2-zAhAGgqeIhrh8VtsEiNzFHqHMVc8BD48TOj4c';
-	$range = 'Names Data!A2:B';
-	$response = $service->spreadsheets_values->get($spreadsheetId, $range);
-	$values = $response->getValues();
-
-	//update rows--first [] = row, second []
-	$values[0][0] = 'Updated';
-	$values[0][1] = 'Row'; 
-	$body = new Google_Service_Sheets_ValueRange(array(
-	  'values' => $values
-	));
-	$params = array(
-	  'valueInputOption' => "RAW"
-	);
-	$result = $service->spreadsheets_values->update($spreadsheetId, $range,
-	    $body, $params);
-
-	//clear specified row(s)--deletes but leaves gap
-	$values[1][0] = '';
-	$values[1][1] = ''; 
-	$body = new Google_Service_Sheets_ValueRange(array(
-	  'values' => $values
-	));
-	$params = array(
-	  'valueInputOption' => "RAW"
-	);
-	$result = $service->spreadsheets_values->update($spreadsheetId, $range,
-	    $body, $params);
-
-	//append rows to end of the current data--have to leave this at end or can't clear specified row--can't find the number
-	$values = array(
-	    array('Appended', 'Row')
-	    // Additional rows ...
-	);
-	$body = new Google_Service_Sheets_ValueRange(array(
-	  'values' => $values
-	));
-	$params = array(
-	  'valueInputOption' => "RAW"
-	);
-	$result = $service->spreadsheets_values->append($spreadsheetId, $range,
-	    $body, $params);
+	header('Location: /home.php');
 }
 
 
